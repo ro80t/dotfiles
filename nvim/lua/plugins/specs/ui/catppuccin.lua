b5eb0c675078b7e0371b50da0f7282131566a4e1
ROBOT <tkata12@outlook.jp>
2026-03-06 18:54:21 +0900

    fix: ui

--- nvim/lua/plugins/specs/ui/catppuccin.lua
+++ nvim/lua/plugins/specs/ui/catppuccin.lua
@@ -2,7 +2,7 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    lazy = false,
+    lazy = true,
     config = function ()
         require("plugins.config.ui.cappuccin")
     end
