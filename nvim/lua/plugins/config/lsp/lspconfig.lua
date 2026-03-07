c862768bbd826d76b0f801062e85f805405684be
ROBOT <tkata12@outlook.jp>
2026-03-07 12:55:17 +0900

    fix: lsp

---
+++ nvim/lua/plugins/config/lsp/lspconfig.lua
@@ -0,0 +1,7 @@
+return function(_, opts)
+    local lspconfig = vim.lsp.config
+    for server, config in pairs(opts.servers) do
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+    end
+end
--- nvim/lua/plugins/specs/lsp/lspconfig.lua
+++ nvim/lua/plugins/specs/lsp/lspconfig.lua
@@ -1,4 +1,18 @@
 return {
     "neovim/nvim-lspconfig",
+
+    dependencies = {
+        "saghen/blink.cmp",
+        { "folke/neoconf.nvim", lazy = true, opts = {} },
+    },
+
     cmd = { "LspInfo", "LspInstall", "LspStart" },
+
+    opts = {
+        servers = {
+--            lua_ls = {}
+        }
+    },
+
+    config = require("plugins.config.lsp.lspconfig")
 }
