22932d7cfcfdb970ae4b61d24e2de13067cab2ac
ROBOT <tkata12@outlook.jp>
2026-03-19 17:26:58 +0900

    add: profile.nvim

--- nvim/lua/core/profile.lua
+++ nvim/lua/core/profile.lua
@@ -1,32 +1,29 @@
 local should_profile = os.getenv("NVIM_PROFILE")
 
-if should_profile ~= nil then
+if should_profile then
     vim.opt.runtimepath:append(vim.fn.stdpath("config") .. "/profile.nvim")
 
     require("profile").instrument_autocmds()
-    if should_profile == "start" then
+    if should_profile:lower():match("^start") then
         require("profile").start("*")
     else
         require("profile").instrument("*")
     end
-
-    local function toggle_profile()
-        local prof = require("profile")
-        if prof.is_recording() then
-            prof.stop()
-            vim.ui.input(
-                { prompt = "Save profile to:", completion = "file", default = "profile.json" },
-                function(filename)
-                    if filename then
-                        prof.export(filename)
-                        vim.notify(string.format("Wrote %s", filename))
-                    end
-                end
-            )
-        else
-            prof.start("*")
-        end
-    end
-
-    vim.keymap.set("", "<f1>", toggle_profile)
 end
+
+local function toggle_profile()
+    local prof = require("profile")
+    if prof.is_recording() then
+        prof.stop()
+        vim.ui.input({ prompt = "Save profile to:", completion = "file", default = "profile.json" }, function(filename)
+            if filename then
+                prof.export(filename)
+                vim.notify(string.format("Wrote %s", filename))
+            end
+        end)
+    else
+        prof.start("*")
+    end
+end
+
+vim.keymap.set("", "<f1>", toggle_profile)
