6bb662f288486131039ecc5ba4a8113359f0f748
ROBOT <tkata12@outlook.jp>
2026-03-31 10:27:18 +0900

    fix: add lazy loading event for various LSP-related plugins

--- nvim/lua/plugins/specs/lsp/mason-lspconfig.lua
+++ nvim/lua/plugins/specs/lsp/mason-lspconfig.lua
@@ -1,5 +1,6 @@
 return {
     "mason-org/mason-lspconfig.nvim",
+    event = { "VeryLazy" },
     dependencies = {
         "mason-org/mason.nvim",
         "neovim/nvim-lspconfig",
--- nvim/lua/plugins/specs/lsp/mason.lua
+++ nvim/lua/plugins/specs/lsp/mason.lua
@@ -1,5 +1,6 @@
 return {
     "mason-org/mason.nvim",
+    event = { "VeryLazy" },
     build = ":MasonUpdate",
     opts = {},
     config = true,
