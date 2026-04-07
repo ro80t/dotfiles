905796b2472a2baac12caaee8e06ef2e18bf0447
ROBOT <tkata12@outlook.jp>
2026-04-07 18:23:58 +0900

    fix: loading queue

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,8 +6,9 @@ vim.g.mapleader = " "
 vim.o.encoding = "utf-8"
 vim.scriptencoding = "utf-8"
 
+require("core.lazy")
+
 require("core.autocmds")
 require("core.keymaps")
-require("core.lazy")
 require("core.options")
 require("core.usercmds")
